8d90e71a16c3355f1414b6028b03e25340b8288b
iiRyan <92063117+iiRyan@users.noreply.github.com>
2024-01-27 01:26:58 +0300

    REST-API-CRUD

---
+++ section-04-rest-crud/01-spring-boot-rest-crud/src/main/java/com/pringourney/restAPI/rest/DemoRestController.java
@@ -0,0 +1,16 @@
+package com.pringourney.restAPI.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+
+    // Add code for the "/hello" endpoint.
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "<h1>Hello Rayan.</h1>";
+    }
+}
