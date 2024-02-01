c86b60ad170b7b5768d894732cc296fe804ec90d
Rayan <92063117+iiRyan@users.noreply.github.com>
2024-02-01 11:11:26 +0300

    SpringMVC Validation- Validate numbers range.

--- section-06-springMVC/thymeleaf/src/main/java/com/spring/demo/thymeleaf/model/Student.java
+++ section-06-springMVC/thymeleaf/src/main/java/com/spring/demo/thymeleaf/model/Student.java
@@ -1,5 +1,7 @@
 package com.spring.demo.thymeleaf.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,10 @@ public class Student {
     @Size(min=1,message = "is required")
     private String lastName;
     private String country;
+
+    @Min(value = 0,message = "must be greater than or equal to zero")
+    @Max(value = 10,message = "must be less than or equal to 10")
+    private int freePass;
 
     public Student() {
     }
@@ -36,5 +42,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(int freePass) {
+        this.freePass = freePass;
     }
 }
