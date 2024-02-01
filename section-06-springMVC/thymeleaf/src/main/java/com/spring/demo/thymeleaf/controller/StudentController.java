36be00bf4ddfc424795a3abdbead64ae271774e8
Rayan <92063117+iiRyan@users.noreply.github.com>
2024-02-01 10:51:25 +0300

    SpingMVC Validation- @InitBinder.

--- section-06-springMVC/thymeleaf/src/main/java/com/spring/demo/thymeleaf/controller/StudentController.java
+++ section-06-springMVC/thymeleaf/src/main/java/com/spring/demo/thymeleaf/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.spring.demo.thymeleaf.controller;
 import com.spring.demo.thymeleaf.model.Student;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +17,21 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+
+    // add an initbinder ... to convert trim input string
+    // remove leading trailing whitespace
+    // resolve issue form our validation
+
+    // This method will be called for every web request coming into our controller.
+    @InitBinder
+    public void iniBinder(WebDataBinder dataBinder){
+
+        // StringTrimmerEditor Remove whitespace - leading and trailing."Ëxtra whitespace"
+        // when pass true - means trim empty string to null(convert the whitespace null).
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     // from the properties that you defined.
     @Value("${countries}")
@@ -32,9 +50,11 @@ public class StudentController {
         return "student-form";
     }
 
+
     @PostMapping("/processStudentForm")
     public String processForm(@Valid @ModelAttribute("student") Student theStudent, BindingResult bindingResult) {
 
+        System.out.println("Last Name: |" + theStudent.getLastName() + "|");
         if (bindingResult.hasErrors()) {
             return "student-form";
         } else {
