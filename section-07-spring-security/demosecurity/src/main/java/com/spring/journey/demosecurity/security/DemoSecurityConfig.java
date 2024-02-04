388bc3c20b36cc34f8198554df783354b1165dfc
Rayan <92063117+iiRyan@users.noreply.github.com>
2024-02-04 12:01:00 +0300

    initial commit

---
+++ section-07-spring-security/demosecurity/src/main/java/com/spring/journey/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.spring.journey.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                // Any request to the app must be authenticated.
+                .anyRequest()
+                .authenticated())
+                .formLogin(form -> // We are customizing the form login process.
+                form.loginPage("/showMyLoginPage") // The controller
+                        .loginProcessingUrl("/authenticationTheUser") // Login form should POST data to this URL for
+                                                                      // processing.
+                        .permitAll() // Allow everyone to see login page no need to be logged in.
+                );
+        return http.build();
+    }
+}
